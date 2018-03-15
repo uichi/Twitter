247abe4f2c69febca5b09ae6b74be09174d4a763
Uichi <ykonishi@protonmail.com>
2018-03-16 01:53:24 +0900

    Add the posting tweet.

---
+++ post_tweet.py
@@ -0,0 +1,12 @@
+import tweepy
+
+CONSUMER_KEY    = ""
+CONSUMER_SECRET = ""
+ACCESS_KEY      = ""
+ACCESS_SECRET   = ""
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+api.update_status(status='テストつぶやき')
